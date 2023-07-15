3f59dfd2aa92e5279399affda5e0054bb42513cf
cedricksupan <cedrick.supan@volenday.com>
2023-07-16 00:21:58 +0800

    remove percentage in voter

--- static/js/showresult.js
+++ static/js/showresult.js
@@ -7,15 +7,7 @@ $(document).ready(function(){
     socket.on('showResult', function(data){
 
         let question = data.question.charAt(0).toUpperCase() + data.question.slice(1);
-        document.getElementById('results-question').innerHTML = ""+question+"";
-
-        let options = "";
-        for(let i = 0; i < data.options.length; i++){
-            options += "<p id='results-option' class='lead'>"+data.options[i].option+"</p>";
-            options += "<p id='results-vote' class='lead'>"+data.options[i].vote+"</p>";
-           
-        }
-        document.getElementById('options').innerHTML = options;
+        document.getElementById('results-question').innerHTML = "<h1>Submitted</h1>";
     });
 
     socket.on('updatedVote', function(){
